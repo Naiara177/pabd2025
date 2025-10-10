87e43b66208a0ae361162d72dc648bdc3e0547c0
Naiara177 <nayarabarrros94@gmail.com>
2025-10-10 12:13:52 +0000

    Aula 26/09 - composição, Encapsulamento,getters/setters e decorator

--- conta.py
+++ conta.py
@@ -1,42 +1,65 @@
-class conta:
-    def __init__(self, titular, agencia, numero, pix, saldo):
-        self.titular = titular
-        self.agencia = agencia
-        self.numero = numero
-        self.pix = pix
-        self.saldo = saldo
+from historico import Historico
 
-    def deposita(self, valor):
-        self.saldo += valor
-    def saca(self, valor):
-        self.saldo -= valor
-def extrato(self):
-        print(f'Titular: {self.titular}')
-        print(f'Agencia: {self.agencia}')
-        print(f'Numero: {self.numero}')
-        print(f'Pix: {self.pix}')
-        print(f'Saldo: {self.saldo:.2f}')
+class Conta:
+	def __init__(self, cliente, agencia, numero, pix, saldo):
+		self.cliente = cliente # agregação
+		self.agencia = agencia
+		self.numero = numero
+		self.pix = pix
+		self._saldo = saldo
+		self.historico = Historico() # composição
 
+	## Decorator - property
+	@property
+	def saldo(self):
+		return self._saldo
+	
+	# Não se aplica para o atributo 'saldo'
+	# @saldo.setter
+	# def saldo(self, saldo):
+	# 	if(saldo < 0):
+	# 		print('Saldo não pode ser negativo!')
+	# 	else:
+	# 		self._saldo = saldo
 
-def saca(self, valor):
-    if self.saldo < valor:
-        return False
-    else:
-        self.saldo -= valor
-        return True
-    
-def deposita(self, valor):
-    self.saldo += valor
-    return True     
+	## getters/setters
+	def get_saldo(self):
+		return self._saldo
 
+	# Não se aplica para o atributo 'saldo'
+	# def set_saldo(self, saldo):
+	# 	if(saldo < 0):
+	# 		print('Saldo não pode ser negativo!')
+	# 	else:
+	# 		self._saldo = saldo
 
+	def deposita(self, valor):
+		self._saldo += valor
+		self.historico.transacoes.append(f"✅ Depósito de R$ {valor:.2f}")
 
-def transfere(self, destino, valor):
-    if self.saca(valor):
-        destino.deposita(valor)
-        return True
-    else:
-        return False
-from conta import Conta
+	def saca(self, valor):
+		if self._saldo < valor:
+			self.historico.transacoes.append(
+					f"❌ Saldo insuficiente. Saque: R$ {valor:.2f} - Saldo R$ {self._saldo:.2f}"
+			)
+			return False
+		else:
+			self._saldo -= valor
+			self.historico.transacoes.append(f"⛔ Saque de R$ {valor:.2f}")
+			return True
 
+	def extrato(self):
+		self.historico.transacoes.append(f"❗ Extrato. Saldo R$ {self._saldo:.2f}")
+		print(
+				f"Titular: {self.cliente.nome}\nCPF: {self.cliente.cpf}\nAgência: {self.agencia}\nNúmero: {self.numero}\nPIX: {self.pix}\nSaldo: {self._saldo:.2f}\n"
+		)
 
+	def transfere(self, destino, valor):
+		self.historico.transacoes.append(
+			f"❗ Transferência para {destino.cliente.nome}"
+		)
+		if self.saca(valor):
+			destino.deposita(valor)
+			return True
+		else:
+			return False
--- main.py
+++ main.py
@@ -1,80 +1,123 @@
-
+## Programação com Acesso a Banco de Dados
 # Revisão de Orientação a Objetos
-# Naiara
+# Prof. Guilherme Leal Santos
 
-#Aula 19/09 -Orientação a Objetos
-from conta import Conta         
 
-conta1 = Conta('Elvis Presley', 1, 123 , 123450000, 'elvis@gamil.com')
+from conta import Conta
+from cliente import Cliente
+
+# Aula 26/09 - Agregação, Composição, Modificadores de Acesso
+cliente1 = Cliente("Elvis Presley", "111.222.333-44")
+conta1 = Conta(cliente1, 1, 123, "elvis@gmail.com", 10000)
 conta1.extrato()
-conta1.depositar(100)
-conta1.sacar(50)
+conta1.saca(500)
+conta1.deposita(300)
 
-
-conta2 = conta1
+cliente2 = Cliente("Jonhny Cage", "222.333.444-55")
+conta2 = Conta(cliente2, 2, 234, "jonhnny@outlook.com", 5000)
 conta2.extrato()
-conta1.extrato
+conta2.saca(100)
+conta2.deposita(600)
 
-if(conta1.saca(100)) :
-    print ('OK!')
+conta1.transfere(conta2, 2000)
+conta2.saca(10000)
+
+conta1.historico.imprime()
+conta2.historico.imprime()
+
+# sem decorator
+conta1.set_saldo(-100)
+print(conta1.get_saldo()) #getter
+print(conta1.get_saldo()*1.1 + conta2.get_saldo()*0.9)
+
+# com decorator
+conta1.saldo = -100
+print(conta1.saldo) #getter
+print(conta1.saldo*1.1 + conta2.saldo*0.9)
+
+# Aula 19/09 - Orientação a Objetos
+
+"""
+cliente1 = Cliente('Elvis Presley', '111.222.333-44')
+conta1 = Conta(cliente1, 1, 123, 'elvis@gmail.com', 12345678)
+conta1.extrato()
+# conta1.deposita(100)
+# conta1.extrato()
+
+# conta2 = conta1
+# conta2.extrato()
+# conta2.saca(100)
+# conta2.extrato()
+# conta1.extrato()
+
+# if(conta1.saca(1000)):
+#     print('OK')
+# else:
+#     print('Tá Liso')
+
+cliente2 = Cliente('Jonhny Cage', '222.333.444-55')
+conta2 = Conta(cliente2, 2, 234, 'jonhnny@outlook.com', 234567)
+conta2.extrato()
+
+if(conta2.transfere(conta1, 1000)):
+    print('OK')
 else:
-    print('Saldo insuficiente!')
-''''''
-#frutas = ['Maçã', 'Banana', 'Laranja']
-#print(frutas)
-#print(frutas[0]) #pra pedir a ordem  e [ numero]
-#print(f'Tamanho: {len(frutas)}') #pedir tamanho e com a função len
+    print('Tá liso')
 
-#frutas.append('Uva') #inserir um elemento
-#print(frutas)
+"""
 
-#frutas.insert(1, 'Abacaxi') #para inserir a ordem e o elemento
-#print(frutas)
+"""
+# Aula 12/092023 - Listas e Funções Lambda
+frutas = ['Maçã', 'Banana', 'Laranja']
+print(frutas)
+print(frutas[0])
+print(f'Tamanho: {len(frutas)}')
 
-# - remove o ultimo elemento da lista
-#frutas.pop() # remove o último elemento da lista
-#frutas.pop(0)  # remove o elemento do indice 0
-#frutas.remove('Laranja')
-#print(f'Removido: {frutas}')
-#print(frutas)
+frutas.append('Uva')
+print(frutas)
 
+frutas.insert(0, 'Abacaxi')
+print(frutas)
 
-#numeros = [3,1,4,1,5,9,2,6,5,3]
-#print(numeros)
+# -> Remove último elemento da lista
+# fruta = frutas.pop() 
+# -> Remove elemento do índice 0
+# fruta = frutas.pop(0)
+frutas.remove('Laranja')
+# print(f'Removido: {fruta}')
+print(frutas)
+
+numeros = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+print(numeros)
 
 # Ordenar - crescente
-#numeros_ord_c = sorted(numeros)
-#print(f'Lista Ordenada (c): {numeros_ord_c}')
+numeros_ord_c = sorted(numeros)
+print(f'Lista ordenada (c): {numeros_ord_c}')
 
-#Ordenar - decrecente
-#numeros_ord_d = sorted(numeros, reverse=True)
-#print(f'Lista Ordenada (d): {numeros_ord_d}')
+# Ordenar - decrescente
+numeros_ord_d = sorted(numeros, reverse=True)
+print(f'Lista ordenada (d): {numeros_ord_d}')
 
-#numeros_dobrados = []
-#for n in numeros:
- #       numeros_dobrados.append(n*2)
-#print(numeros_dobrados)
-#numeros_dobrados = list(map(lambda n: n*2, numeros)) 
-#print(numeros_dobrados)
+# numeros_dobrados = []
+# for n in numeros:
+#     numeros_dobrados.append(n*2)
+numeros_dobrados = list(map(lambda n: n*2, numeros))
+print(numeros_dobrados)
 
-#numeros_filtrados = [ ]
-#for n in numeros:
- #   if n > 4:
-  #      numeros_filtrados.append(n)
-#print(numeros_filtrados)
+# numeros_filtrados = []
+# for n in numeros:
+#     if n > 4:
+#         numeros_filtrados.append(n)
+numeros_filtrados = list(filter(lambda n: n > 4, numeros))
+print(numeros_filtrados)
 
-#numeros_filtrados = list(filter(lambda n: n > 4, numeros))
-#print(numeros_filtrados)
+soma = 0
+for n in numeros:
+    soma += n
+print(soma)
 
-#soma = 0 
-#for n in numeros:
- #   soma += n
-#print(f'Soma: {soma}')
+from functools import reduce
 
-#print(soma)
-
-#from functools import reduce
-
-#soma = reduce(lambda soma, n: soma + n, numeros) # é o valor inicial  do acumulador
-#print(soma)  
-''''''
+soma = reduce(lambda soma, n: soma + n, numeros)
+print(soma)
+"""
